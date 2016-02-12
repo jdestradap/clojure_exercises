3ac77ddfd0c2312516453108799d09146d9c8be0
jdestradap <jdestradap@gmail.com>
2016-02-12 01:42:09 -0500

    day three first part.

--- adventofcode.clj
+++ adventofcode.clj
@@ -228,28 +228,34 @@
   [x y map]
 ())
 
+(ns clojure-noob.core
+  (:require [clojure.core.match :refer :all]))
 
 (defn move
   [sym]
   (match [sym]
-         ["^"] (seq [0 1])
-         ["<"] (seq [-1 0])
-         ["v"] (seq [0 -1])
-         [">"] (seq [1 0])))
+         [\^] (seq [0 1])
+         [\<] (seq [-1 0])
+         [\v] (seq [0 -1])
+         [\>] (seq [1 0])
+         [_]  (seq [0 0])))
 
-(defn next
+(defn ne
   [[c1 c2] [m1 m2]]
     (seq [(+ c1 m1) (+ c2 m2)]))
-
-
 
 (defn give
   [houses next]  
   (let [[x y] next] (update houses [(keyword (Integer/toString x)) (keyword (Integer/toString y))] (fnil inc 0)))
 )
 
-(loop [movements ["<" "v" ">" "^"] houses {} current (seq [0 0])]
-  (if (empty? movements)
-    houses
-    (let [[part & remaining] movements n (ne current (move part))]
-      (recur remaining (give houses n) n))))
+(defn houses-with-presents
+  [m]
+  (loop [movements m houses {} current (seq [0 0])]
+    (if (empty? movements)
+      houses
+      (let [[part & remaining] movements n (ne current (move part))]
+        (recur remaining (give houses n) n)))))
+
+
+(count (houses-with-presents (seq (char-array(clojure.string/trim-newline(slurp "/Users/jdestradap/projects/clojure-hello-world/clojure-noob/src/clojure_noob/input_day_3.txt"))))))
